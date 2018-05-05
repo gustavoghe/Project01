1ee8e423538a71438e04de58920e77a1f24f4be0
gustavoghe <gustavoghe@gmail.com>
2018-05-05 00:44:27 -0300

    Finished

--- AdressBook/AdressBook/Program.cs
+++ AdressBook/AdressBook/Program.cs
@@ -14,100 +14,100 @@ namespace AdressBook
             List<Contact> AdressBook = new List<Contact>();
 
             DateTime Example = new DateTime(1980, 2, 22);
-
+            int i = 0;
             AdressBook.Add(new Contact("Gustavo", "Encinas", "55-2423757", Example, 38));
             AdressBook.Add(new Contact("Kenion", "Professor", "55-2423757", Example, 38));
 
-            Console.WriteLine("Welcome to AddressBook APP");
-            Console.WriteLine("What would you like to do? Type the number of the option.");
-
-            Console.WriteLine("1) List all contacts in the AddressBook");
-            Console.WriteLine("2) Add a new contact to the AddressBook");
-            Console.WriteLine("3) Delete a contact");
-            Console.WriteLine("4) Edit a contact");
-            Console.WriteLine("5) Exit the APP");
-           
-            int Option = int.Parse(Console.ReadLine());
-            
-            switch (Option)
+            while (i == 0)
             {
-                case 1:
-                    {
-                    Console.WriteLine("You chose option 1");
-                        foreach ( var contact in AdressBook)
+                Console.Clear();
+                Console.WriteLine("Welcome to AddressBook APP");
+                Console.WriteLine("What would you like to do? Type the number of the option.");
+
+                Console.WriteLine("1) List all contacts in the AddressBook");
+                Console.WriteLine("2) Add a new contact to the AddressBook");
+                Console.WriteLine("3) Delete a contact");
+                Console.WriteLine("4) Edit a contact");
+                Console.WriteLine("5) Exit the APP");
+
+                int Option = int.Parse(Console.ReadLine());
+
+                switch (Option)
+                {
+                    case 1:
                         {
-                            Console.WriteLine("Name: {0} Surname: {1} Tel: {2} DOB: {3} Age {4}",contact.FirstName, contact.SecondName, contact.PhoneNumber,contact.BirthDay, contact.Age);
-                        }
-                    Console.ReadLine();
-                    break;
-                    }
-
-                case 2:
-                    {
-                        Console.WriteLine("You chose option 2");
-
-                        Console.WriteLine("Type First Name:");
-                        string firstn = Console.ReadLine();
-
-                        Console.WriteLine("Type Second Name:");
-                        string secondn = Console.ReadLine();
-
-                        Console.WriteLine("Type Cell Phone (Format: 55.1234567:");
-                        string phone = Console.ReadLine();
-
-                        Console.WriteLine("Type Date of Birth (Format dd.mm.yyyy:");
-                        DateTime dob = DateTime.Parse(Console.ReadLine());
-
-                        DateTime now = DateTime.Today;
-                        int age = now.Year - dob.Year;
-                        Console.WriteLine("Age Computed as {0}", age);
-                        Console.ReadLine();
-
-                        AdressBook.Add(new Contact(firstn, secondn, phone, dob, age));
-                        break;
-                    }
-
-                case 3:
-                    {
-                        Console.WriteLine("You chose option 3");
-                        Console.WriteLine("Type first name of contact you want to delete:");
-                        string subject = Console.ReadLine();
-
-                        foreach (var contact in AdressBook)
-                        {
-                            if (contact.FirstName == subject)
+                            Console.WriteLine("You chose option 1");
+                            foreach (var contact in AdressBook)
                             {
-                                AdressBook.Remove(contact);
+                                Console.WriteLine("Name: {0} Surname: {1} Tel: {2} DOB: {3} Age {4}", contact.FirstName, contact.SecondName, contact.PhoneNumber, contact.BirthDay, contact.Age);
                             }
-                            
+                            Console.ReadLine();
+                            break;
                         }
-                        break;
-                    }
 
-                case 4:
-                    {
-                        Console.WriteLine("You chose option 2");
-                        break;
-                    }
+                    case 2:
+                        {
+                            Console.WriteLine("You chose option 2");
 
-                case 5:
-                    {
-                        Console.WriteLine("You chose option 2");
-                        break;
-                    }
+                            Console.WriteLine("Type First Name:");
+                            string firstn = Console.ReadLine();
 
-                default:
-                    {
-                        Console.WriteLine("Please choose between 1 and 5 as a valid option");
-                        break;
-                    }
+                            Console.WriteLine("Type Second Name:");
+                            string secondn = Console.ReadLine();
+
+                            Console.WriteLine("Type Cell Phone (Format: 55.1234567:");
+                            string phone = Console.ReadLine();
+
+                            Console.WriteLine("Type Date of Birth (Format dd.mm.yyyy:");
+                            DateTime dob = DateTime.Parse(Console.ReadLine());
+
+                            DateTime now = DateTime.Today;
+                            int age = now.Year - dob.Year;
+                            Console.WriteLine("Age Computed as {0}", age);
+                            Console.ReadLine();
+
+                            AdressBook.Add(new Contact(firstn, secondn, phone, dob, age));
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            Console.WriteLine("You chose option 3");
+                            Console.WriteLine("Type first name of contact you want to delete:");
+                            string subject = Console.ReadLine();
+                            int indexsubject = AdressBook.FindIndex(subject);
+                            Console.WriteLine("{0}", indexsubject);
+
+                            
+                            break;
+                        }
+
+                    case 4:
+                        {
+                            Console.WriteLine("Under Construction");
+                            break;
+                        }
+
+                    case 5:
+                        {
+                            Console.WriteLine("You chose option 5, do you want to leave?");
+                            Console.ReadLine();
+                            i++;
+                            break;
+                        }
+
+                    default:
+                        {
+                            Console.WriteLine("Please choose between 1 and 5 as a valid option");
+                            break;
+                        }
+
+                }
+
+
+
 
             }
-
-
-
-
-
         }
     }
 }
---
+++ ArrayExercise/ArrayExercise/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExercise
+{
+    class Program
+    {
+
+        //int[] InitialArray = new int[0];
+
+        static void Main(string[] args)
+        {
+            int[] InitialArray = new int[1];
+            InitialArray[0] = 1;
+            
+
+            int h = 1;
+            while (h > 0)
+            {
+                Console.WriteLine("Welcome to Array Exercise:");
+                Console.WriteLine("Please choose an option from the menu by typing the number:");
+                Console.WriteLine("1) Create an Array and populate it with random numbers");
+                Console.WriteLine("2) Display Array in original order");
+                Console.WriteLine("3) Display Array in crescent order");
+                Console.WriteLine("4) Display Array in decrescent order");
+                Console.WriteLine("5) Exit Program");
+                //Console.WriteLine("1)");
+
+                int Option = int.Parse(Console.ReadLine());
+
+                switch (Option)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Option 1");
+                            Console.WriteLine("Define a size for the array between 5 and 10 elements");
+                            int Size = int.Parse(Console.ReadLine());
+                            int[] SupportArray = CreateArray(Size);
+                            Array.Resize(ref InitialArray, SupportArray.Length);
+
+                            for (int i = 0; i < SupportArray.Length; i++)
+                            {
+                                InitialArray[i] = SupportArray[i];
+                            }
+
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            DisplayArray(InitialArray);
+                            Console.ReadLine();
+                            break;
+                        }
+                    case 3:
+                        {
+                            DisplayArray(SortCrescent(InitialArray));
+                            Console.ReadLine();
+                            break;
+                        }
+                    case 4:
+                        {
+                            DisplayArray(SortDecrescent(InitialArray));
+                            Console.ReadLine();
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("Press anykey to exit");
+                            Console.ReadLine();
+                            h++;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Choose valid option");
+                            break;
+                        }
+                }
+            }
+        }
+
+        private static int[] CreateArray(int size)
+        {
+            int[] subjectarray = new int[size];
+            Random r = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                subjectarray[i] = r.Next(1, 100);
+            }
+
+            DisplayArray(subjectarray);
+            Console.ReadLine();
+            return subjectarray;
+            
+        }
+
+        private static void DisplayArray(int[] myarray)
+        {
+            if (myarray.Length > 10)
+            {
+                Console.WriteLine("Index  | Member Value");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("{0}      | {1}", i, myarray[i]);
+                }
+
+                for (int i = 11; i < myarray.Length; i++)
+                {
+                    Console.WriteLine("{0}     | {1}", i, myarray[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Index | Member Value");
+
+                for (int i = 0; i < myarray.Length; i++)
+                {
+                    Console.WriteLine("{0}     | {1}", i, myarray[i]);
+                }
+            }
+        }
+
+        private static int[] SortCrescent(int[] arr01)
+        {
+            int[] Sorted = new int[arr01.Length];
+           // bool check = false;
+
+            for (int i = 0; i < arr01.Length; i++)
+            {
+                for (int j = i+1; j < arr01.Length; j++)
+                {
+                    if (arr01[i] > arr01[j])
+                    {
+                        int x = arr01[j];
+                        int y = arr01[i];
+
+                        arr01[i] = x;
+                        arr01[j] = y;
+                        
+                    }                    
+                    
+                }
+            }
+
+            return arr01;
+
+        }
+
+        private static int[] SortDecrescent(int[] arr01)
+        {
+            int[] Sorted = new int[arr01.Length];
+            // bool check = false;
+
+            for (int i = 0; i < arr01.Length; i++)
+            {
+                for (int j = i + 1; j < arr01.Length; j++)
+                {
+                    if (arr01[i] < arr01[j])
+                    {
+                        int x = arr01[j];
+                        int y = arr01[i];
+
+                        arr01[i] = x;
+                        arr01[j] = y;
+
+                    }
+
+                }
+            }
+
+            return arr01;
+
+        }
+
+
+    }
+}
